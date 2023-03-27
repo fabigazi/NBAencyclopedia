225e2bba45ab50a71b086aa7a710123c6ba835e7
fabigazi <fgaziano98@gmail.com>
2023-03-27 11:05:20 -0400

    customtkinter

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import login_frame
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    login_frame.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
