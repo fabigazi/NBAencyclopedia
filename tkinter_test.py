eb99d71a3f4408026cd661a97a0fa600174889ec
John O'Connell <oconnell.j@northeastern.edu>
2023-04-17 16:33:36 -0400

    implemented menu page in tkinter_test.py

--- tkinter_test.py
+++ tkinter_test.py
@@ -12,9 +12,60 @@ def username_exists(cur, username):
         return 1
     else:
         return 0
-    
 
-def login(cur, username, password):
+def on_closing(root):
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        #wn.destroy()
+        root.destroy()
+        quit
+
+def main_menu(cnx, cur, root):
+    root.withdraw()
+
+    # Create new window
+    wn = Toplevel(root)
+    # Create new frame in window
+    frame2 = Frame(wn)
+
+    # Set window specifications and location
+    window_width = 750
+    window_height = 500
+    screen_width = wn.winfo_screenwidth()
+    screen_height = wn.winfo_screenheight()
+    x_coor = screen_width/2 - window_width/2
+    y_coor = screen_height/2 - window_height/2
+    wn.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coor, y_coor))
+
+    # Creating widgets
+    menu_label = Label(frame2, text = 'Main Menu', font = ('Arial Bold', 32))
+    exit_btn = Button(frame2, text = 'Exit', font = ('Arial', 14), 
+                        command = lambda : [wn.destroy(), root.deiconify()])
+    btn1 = Button(frame2, text = 'View / Search Players', font = ('Arial', 14), 
+                    command = lambda : print('ok') )
+    btn2 = Button(frame2, text = 'View / Search Teams', font = ('Arial', 14), 
+                    command = lambda : print('ok') )
+    btn3 = Button(frame2, text = 'View / Search All Star Teams', font = ('Arial', 14), 
+                    command = lambda : print('ok') )
+    btn4 = Button(frame2, text = 'View / Search Fantasy Teams', font = ('Arial', 14), 
+                    command = lambda : print('ok') )
+    btn5 = Button(frame2, text = 'Create a Team', font = ('Arial', 14), 
+                    command = lambda : print('ok') )
+        
+    # Placing widgets on the screen
+    menu_label.grid(row=1, column=0, columnspan=2, sticky='NEWS', pady=(20,30))
+    btn1.grid(row=2, column=0, columnspan=2,sticky='NEWS')
+    btn2.grid(row=3, column=0, columnspan=2, sticky='NEWS')
+    btn3.grid(row=4, column=0, columnspan=2, sticky='NEWS')
+    btn4.grid(row=5, column=0, columnspan=2, sticky='NEWS')
+    btn5.grid(row=6, column=0, columnspan=2, sticky='NEWS')
+    exit_btn.grid(row=7, column=0, columnspan=2, sticky='NEWS')
+
+    frame2.pack(anchor = tk.CENTER)
+
+    wn.protocol("WM_DELETE_WINDOW", lambda : on_closing(root))
+    wn.mainloop()
+
+def login(cnx, cur, root, username, password):
 
     if not username or not password:
         messagebox.showerror(title = 'Login Invalid', message = 'No username or password entered!')
@@ -30,7 +81,7 @@ def login(cur, username, password):
             messagebox.showerror(title = 'Login Invalid', message = 'Incorrect password!')
 
         else:
-            print('success!')
+            main_menu(cnx, cur, root)
 
     return
 
@@ -114,8 +165,8 @@ def start_screen(cnx, cur, root):
     username_entry = tk.Entry(frame)
     password_label = tk.Label(frame, text = 'Password', font = ('Arial', 14))
     password_entry = tk.Entry(frame, show = '*')
-    login_button = tk.Button(frame, text = 'Login', font = ('Arial', 14), 
-                             command = lambda : [login(cur, username_entry.get(), password_entry.get()), clear_text()])
+    login_button = tk.Button(frame, text = 'Login', font = ('Arial', 14),
+                             command = lambda : [login(cnx, cur, root, username_entry.get(), password_entry.get())])
     register_button = tk.Button(frame, text = 'Register', font = ('Arial', 14), 
                                 command = lambda : [register(cur, root), cnx.commit()])
     logo = ImageTk.PhotoImage(Image.open('nba_logo.png'))
