dc0fd8bf83b2bff45b92488c467c2546eafc56de
fabigazi <fgaziano98@gmail.com>
2023-04-18 15:41:12 -0400

    Search button partail functionality and stored procedure call

--- player_search_window.py
+++ player_search_window.py
@@ -8,13 +8,12 @@ from tkinter import messagebox, ttk
 import pandas as pd
 from PIL import Image, ImageTk
 
-
 customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
 def open_window(cnx, cur, root, window):
-    #root.withdraw()
+    # root.withdraw()
     window.withdraw()
     # Create new window
     wn = Toplevel(root)
@@ -41,40 +40,44 @@ def open_window(cnx, cur, root, window):
         columns={'player_name': 'Name', 'season_year': 'Year', 'position': 'Position', 'team': 'Team'})
 
     # Createing widgets
-    tab_view_filters = customtkinter.CTkTabview(frame3, width=150, height=400)
-    tab_view_filters.grid(row=0, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
-    tab_view_filters.add("Player Search")
+    frame_filters = customtkinter.CTkFrame(frame3, width=150, height=400)
+    frame_filters.grid(row=0, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+    #frame_filters.add("Player Search")
+    Label = customtkinter.CTkLabel(frame_filters, text="Player Search:")
+
+    Label.grid(row=0, column=0, padx=20, pady=(8, 8))
 
     # TODO: fill with data from DB
-    year_dd = customtkinter.CTkOptionMenu(tab_view_filters.tab("Player Search"),
+    year_dd = customtkinter.CTkOptionMenu(frame_filters,
                                           dynamic_resizing=True,
                                           values=["Any Year", "2023", "2022", "2021"])
 
-    year_dd.grid(row=0, column=0, padx=20, pady=(10, 10))
+    year_dd.grid(row=1, column=0, padx=20, pady=(10, 10))
 
     # TODO: fill with data from DB
-    position_dd = customtkinter.CTkOptionMenu(tab_view_filters.tab("Player Search"),
+    position_dd = customtkinter.CTkOptionMenu(frame_filters,
                                               dynamic_resizing=True,
                                               values=["Any Position", "SG", "SF", "PF"])
 
-    position_dd.grid(row=1, column=0, padx=20, pady=(10, 10))
+    position_dd.grid(row=2, column=0, padx=20, pady=(10, 10))
 
-    player_name_entry = customtkinter.CTkEntry(tab_view_filters.tab("Player Search"),
+    player_name_entry = customtkinter.CTkEntry(frame_filters,
                                                placeholder_text="Player Name")
-    player_name_entry.grid(row=2, column=0, padx=20, pady=(10, 10))
+    player_name_entry.grid(row=3, column=0, padx=20, pady=(10, 10))
 
-    team_name_entry = customtkinter.CTkEntry(tab_view_filters.tab("Player Search"),
+    team_name_entry = customtkinter.CTkEntry(frame_filters,
                                              placeholder_text="Team Name")
-    team_name_entry.grid(row=3, column=0, padx=20, pady=(10, 10))
+    team_name_entry.grid(row=4, column=0, padx=20, pady=(10, 10))
 
-    search_button = customtkinter.CTkButton(tab_view_filters.tab("Player Search"), text="Search",
-                                            command=lambda: [run_search(year_dd, position_dd, player_name_entry,
-                                                                        team_name_entry)])
-    search_button.grid(row=4, column=0, padx=20, pady=(10, 10))
+    search_button = customtkinter.CTkButton(frame_filters, text="Search",
+                                            command=lambda: [run_search(cur, year_dd.get(), position_dd.get(),
+                                                                        player_name_entry.get(),
+                                                                        team_name_entry.get())])
+    search_button.grid(row=5, column=0, padx=20, pady=(10, 10))
 
-    back = customtkinter.CTkButton(tab_view_filters.tab("Player Search"), text="Back",
-                                            command=lambda: [wn.destroy(), window.deiconify()])
-    back.grid(row=5, column=0, padx=20, pady=(10, 10))
+    back = customtkinter.CTkButton(frame_filters, text="Back",
+                                   command=lambda: [wn.destroy(), window.deiconify()])
+    back.grid(row=6, column=0, padx=20, pady=(10, 10))
 
     # tree view frame
     treeview_frame = ttk.Treeview(frame3, height=18, padding=1, show="headings")
@@ -91,16 +94,37 @@ def open_window(cnx, cur, root, window):
     wn.mainloop()
 
 
-def run_search(year, position, p_name, t_name):
+def run_search(cur, year, position, p_name, t_name):
     # where we should run search with given inputs
     filler = 0
-    print(year.get())
-    print(position.get())
+    print(year)
+    print(position)
     print(p_name)
     print(t_name)
 
+    year_input = year
+    position_input = position
+    p_name_input = p_name
+    t_name_input = t_name
+
+    # empty string vs null
+    if year == "Any Year":
+        year_input = "NULL"
+
+    if position == "Any Position":
+        position_input = "NULL"
+
+    if p_name == "":
+        p_name_input = "NULL"
+
+    if t_name == "":
+        t_name_input = "NULL"
+
+    cur.execute(f"CALL player_search('{year_input}', '{position_input}', '{p_name_input}', '{t_name_input}')")
+
+
 def on_closing(root):
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        #wn.destroy()
+        # wn.destroy()
         root.destroy()
         quit
