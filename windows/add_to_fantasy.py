d29f9d56d52d07895f619da3d0bd95b5ecd4387a
John O'Connell <oconnell.j@northeastern.edu>
2023-04-20 19:19:38 -0400

    implemented end of season teams page

--- windows/add_to_fantasy.py
+++ windows/add_to_fantasy.py
@@ -6,7 +6,7 @@ from tkinter import *
 from tkinter import messagebox, ttk
 import pandas as pd
 
-from windows.create_fantasy import user_fantasy_search
+# from windows.create_fantasy import user_fantasy_search
 
 customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -103,8 +103,8 @@ def open_add_to_fantasy(username, team_name, cnx, cur, root, window, treeview):
     search_button.grid(row=5, column=0, padx=20, pady=(10, 10))
 
     add = customtkinter.CTkButton(frame_filters, text="Add",
-                                   command=lambda: [wn.destroy(), window.deiconify(),
-                                                    user_fantasy_search(cur, username, treeview)])
+                                   command=lambda: [wn.destroy(), window.deiconify()])
+                                                    #user_fantasy_search(cur, username, treeview)])
     add.grid(row=6, column=0, padx=20, pady=(10, 10))
 
     back = customtkinter.CTkButton(frame_filters, text="Back",
--- windows/team_search.py
+++ windows/team_search.py
@@ -25,10 +25,10 @@ def open_team_search(cnx, cur, root, window):
         year_drop_down.append(str(row['season']))
 
     team_drop_down = ["Any Team"]
-    cur.execute('CALL player_search_team_drop_down()')
+    cur.execute('CALL end_of_season_team_drop_down()')
 
     for row in cur.fetchall():
-        team_drop_down.append(str(row['tm']))
+        team_drop_down.append(str(row['type']))
 
     # Set window specifications and location
     window_width = 1100
@@ -42,11 +42,12 @@ def open_team_search(cnx, cur, root, window):
     # initialize variables
     is_on = {}
     df = pd.DataFrame({'team': pd.Series(dtype='str'),
+                       'player': pd.Series(dtype='str'),
                        'season_year': pd.Series(dtype='str')})
 
-    df = df[['team', 'season_year']]
+    df = df[['team', 'player', 'season_year']]
     df = df.rename(
-        columns={'team': 'Team', 'season_year': 'Year'})
+        columns={'team' : 'Team', 'player': 'Player', 'season_year': 'Year'})
 
     # tree view frame
     treeview_frame = ttk.Treeview(frame, height=18, padding=1, show="headings")
@@ -61,7 +62,7 @@ def open_team_search(cnx, cur, root, window):
     frame_filters = customtkinter.CTkFrame(frame, width=150, height=400)
     frame_filters.grid(row=0, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
     # frame_filters.add("Player Search")
-    Label = customtkinter.CTkLabel(frame_filters, text="Team Search:")
+    Label = customtkinter.CTkLabel(frame_filters, text="End of Season Teams Search:")
 
     Label.grid(row=0, column=0, padx=20, pady=(8, 8))
 
@@ -108,23 +109,20 @@ def run_search(cur, year, t_name, treeview):
 
     command = get_command(input, year, "", "", t_name)
     cur.execute(command)
-    # cur.execute(f"CALL player_search('{p_name_input}', '{year_input}', '{position_input}', '{t_name_input}')")
-    df = pd.DataFrame({'player_name': pd.Series(dtype='str'),
-                       'season_year': pd.Series(dtype='str'),
-                       'position': pd.Series(dtype='str'),
-                       'team': pd.Series(dtype='str')})
-    for row in cur.fetchall():
-        new_row = {'player_name': row['player'], 'season_year': row['season'],
-                   'position': row['pos'], 'team': row['tm']}
-        df.loc[len(df)] = new_row
 
-    if not df.empty:
-        df_trimmed = df[["player_name", "season_year", "position", "team"]]
+    df = pd.DataFrame({'team': pd.Series(dtype='str'),
+                       'player_name': pd.Series(dtype='str'),
+                       'season_year': pd.Series(dtype='str')})
+    
+    for row in cur.fetchall():
+        new_row = {'team': row['type'], 'player_name': row['player'],
+                   'season_year': row['season']}
+        df.loc[len(df)] = new_row
 
     for item in treeview.get_children():
         treeview.delete(item)
 
-    for index, row in df_trimmed.iterrows():
+    for index, row in df.iterrows():
         treeview.insert("", tk.END, values=list(row))
 
 
@@ -137,32 +135,10 @@ def on_closing(root):
 
 def get_command(input, year, position, p_name, t_name):
     if input == '':
-        return 'CALL generic_player_search()'
+        return 'CALL generic_eos_search()'
     elif input == 'Y':
-        return f'CALL player_search_year({year})'
-    elif input == 'P':
-        return f'CALL player_search_pos("{position}")'
-    elif input == 'N':
-        return f'CALL player_search_name("{p_name}")'
+        return f'CALL eos_search_year({year})'
     elif input == 'T':
-        return f'CALL player_search_team("{t_name}")'
-    elif input == 'YP':
-        return f'CALL player_search_year_pos({year}, "{position}")'
-    elif input == 'YN':
-        return f'CALL player_search_year_name({year}, "{p_name}")'
-    elif input == 'YT':
-        return f'CALL player_search_year_team({year}, "{t_name}")'
-    elif input == 'PN':
-        return f'CALL player_search_pos_name("{position}", "{p_name}")'
-    elif input == 'PT':
-        return f'CALL player_search_pos_team("{position}", "{t_name}")'
-    elif input == 'NT':
-        return f'CALL player_search_name_team("{p_name}", "{t_name}")'
-    elif input == 'YPN':
-        return f'CALL player_search_year_pos_name({year}, "{position}", "{p_name}")'
-    elif input == 'YPT':
-        return f'CALL player_search_year_pos_team({year}, "{position}", "{t_name}")'
-    elif input == 'PNT':
-        return f'CALL player_search_pos_name_team("{position}", "{p_name}", "{t_name}")'
+        return f'CALL eos_search_team("{t_name}")'
     else:
-        return f'CALL player_search_all({year}, "{position}", "{p_name}", "{t_name}")'
+        return f'CALL eos_search_year_team({year}, "{t_name}")'
